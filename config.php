80cad470322f8e22bab9a2e9deb869f9842abf89
Ankit Kumar Tiwari <50569479+tiwariankit651@users.noreply.github.com>
2021-11-14 16:57:59 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+
+<?php
+
+//start session on web page
+session_start();
+
+//config.php
+
+//Include Google Client Library for PHP autoload file
+require_once 'vendor/autoload.php';
+
+//Make object of Google API Client for call Google API
+$google_client = new Google_Client();
+
+//Set the OAuth 2.0 Client ID
+$google_client->setClientId('1070577137337-tqftpouuljheuugchm1dq506vi9gcih4.apps.googleusercontent.com');
+
+//Set the OAuth 2.0 Client Secret key
+$google_client->setClientSecret('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');
+
+//Set the OAuth 2.0 Redirect URI
+$google_client->setRedirectUri('http://localhost/php_tut/googleloginauthapi/index.php');
+
+// to get the email and profile 
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+?>   
